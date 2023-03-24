adc490062f9956ab26103fe69e8bd5a0b7d17394
rgreen32 <ray.greenm@gmail.com>
2023-03-24 10:02:42 -0500

    completed saveToLocalStorage method

--- js/app.js
+++ js/app.js
@@ -21,10 +21,14 @@ Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
   const newItem = new CartItem(product, quantity)
   this.items.push(newItem)
+  console.log(this.items)
+
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  let cartString = JSON.stringify(state.cart)
+  localStorage.setItem("cart", cartString)
 };
 
 Cart.prototype.removeItem = function(item) {
